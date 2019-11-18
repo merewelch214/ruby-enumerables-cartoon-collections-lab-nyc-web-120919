997e8aad7a87f77f07d9a4a6905b7422fe962214
comical-sandbox-7108 <merewelch@gmail.com>
2019-11-18 20:48:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def find_the_cheese(array)
     if cheese_types.include?(obj) 
       return obj
     end
-    if cheese_types.!include?(obj)
+  end
+  if cheese_types.!include?(obj)
       return nil
-    end
   end
 end
